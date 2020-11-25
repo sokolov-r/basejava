6abe1e47c98ba4617fdc771fe28e97e0076fd071
sokolov-r <sokolov.r.s@list.ru>
2020-11-25 13:12:48 +0300

    correct DataStreamSerializer

--- src/com/basejava/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/basejava/webapp/storage/serializer/DataStreamSerializer.java
@@ -36,22 +36,14 @@ public class DataStreamSerializer implements SerializeStrategy {
                         writeCollection(organizationList, dos, organization -> {
                             dos.writeUTF(organization.getName());
                             String link = organization.getLink();
-                            if (link == null) {
-                                dos.writeUTF("");
-                            } else {
-                                dos.writeUTF(link);
-                            }
+                            dos.writeUTF(link == null ? "" : link);
                             List<Organization.Position> positionList = organization.getPositionList();
                             writeCollection(positionList, dos, position -> {
                                 dos.writeLong(position.getStartDate().toEpochDay());
                                 dos.writeLong(position.getFinishDate().toEpochDay());
                                 dos.writeUTF(position.getJobTitle());
                                 String jobPosition = position.getJobDescription();
-                                if (jobPosition == null) {
-                                    dos.writeUTF("");
-                                } else {
-                                    dos.writeUTF(jobPosition);
-                                }
+                                dos.writeUTF(jobPosition == null ? "" : jobPosition);
                             });
 
                         });
@@ -86,18 +78,14 @@ public class DataStreamSerializer implements SerializeStrategy {
                             List<Organization.Position> positionList = new ArrayList<>();
                             String name = dis.readUTF();
                             String link = dis.readUTF();
-                            if (link.equals("")) {
-                                link = null;
-                            }
+                            link = link.equals("") ? null : link;
                             Organization organization = new Organization(name, link, positionList);
                             readCollection(dis, () -> {
                                 LocalDate startDay = LocalDate.ofEpochDay(dis.readLong());
                                 LocalDate finishDay = LocalDate.ofEpochDay(dis.readLong());
                                 String jobTitle = dis.readUTF();
                                 String jobDescription = dis.readUTF();
-                                if (jobDescription.equals("")) {
-                                    jobDescription = null;
-                                }
+                                jobDescription = jobDescription.equals("") ? null : jobDescription;
                                 positionList.add(new Organization.Position(startDay,
                                         finishDay, jobTitle, jobDescription));
                             });
@@ -128,7 +116,7 @@ public class DataStreamSerializer implements SerializeStrategy {
         }
     }
 
-    private <T> void readCollection(DataInputStream dis, ReaderElements reader) throws IOException {
+    private void readCollection(DataInputStream dis, ReaderElements reader) throws IOException {
         int size = dis.readInt();
         for (int i = 0; i < size; i++) {
             reader.read();
