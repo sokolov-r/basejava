54d072c6b58b417735fed5e2a67ea336bce25caa
sokolov-r <sokolov.r.s@list.ru>
2021-01-13 15:51:24 +0300

    correct MainStream

--- src/com/basejava/webapp/MainStream.java
+++ src/com/basejava/webapp/MainStream.java
@@ -2,16 +2,15 @@ package com.basejava.webapp;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class MainStream {
 
     public static void main(String[] args) {
-        int[] values = {3, 5, 1, 4, 2, 3};
+        int[] values = {3, 5, 1, 4, 2, 3, 5};
         System.out.println(minValue(values));
 
-        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
         System.out.println(oddOrEven(integers));
     }
 
@@ -23,28 +22,22 @@ public class MainStream {
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        final int[] sum = new int[1];
-        List<Integer> list = integers.stream()
-                .peek(integer -> sum[0] = sum[0] + integer)
-                .filter(new Predicate<Integer>() {
-                    @Override
-                    public boolean test(Integer integer) {
-                        if (sum[0] % 2 == 0) {
-                            if (integer % 2 == 0) {
-                                return true;
-                            } else {
-                                return false;
-                            }
-                        } else {
-                            if (integer % 2 == 0) {
-                                return false;
-                            } else {
-                                return true;
-                            }
-                        }
-                    }
-                })
-                .collect(Collectors.toList());
+        int s = 0;
+        for (Integer i : integers) {
+            s = s + i;
+        }
+
+        final int sum = s;
+        List<Integer> list;
+        if (sum % 2 == 0) {
+            list = integers.stream()
+                    .filter(integer -> integer % 2 == 0)
+                    .collect(Collectors.toList());
+        } else {
+            list = integers.stream()
+                    .filter(integer -> integer % 2 != 0)
+                    .collect(Collectors.toList());
+        }
         return list;
     }
 }
