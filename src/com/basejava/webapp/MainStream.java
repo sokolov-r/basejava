5070fcb171784a903735bd613b42b8dd3fc0d4d0
sokolov-r <sokolov.r.s@list.ru>
2021-01-14 12:30:37 +0300

    correct MainStream

--- src/com/basejava/webapp/MainStream.java
+++ src/com/basejava/webapp/MainStream.java
@@ -10,7 +10,7 @@ public class MainStream {
         int[] values = {3, 5, 1, 4, 2, 3, 5};
         System.out.println(minValue(values));
 
-        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
         System.out.println(oddOrEven(integers));
     }
 
@@ -22,22 +22,17 @@ public class MainStream {
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        int s = 0;
-        for (Integer i : integers) {
-            s = s + i;
-        }
+        int sum = integers.stream()
+                .reduce(0, Integer::sum);
 
-        final int sum = s;
-        List<Integer> list;
-        if (sum % 2 == 0) {
-            list = integers.stream()
-                    .filter(integer -> integer % 2 == 0)
-                    .collect(Collectors.toList());
-        } else {
-            list = integers.stream()
-                    .filter(integer -> integer % 2 != 0)
-                    .collect(Collectors.toList());
-        }
-        return list;
+        return integers.stream()
+                .filter(integer -> {
+                    if (sum % 2 == 0) {
+                        return (integer % 2 != 0);
+                    } else {
+                        return integer % 2 == 0;
+                    }
+                })
+                .collect(Collectors.toList());
     }
 }
